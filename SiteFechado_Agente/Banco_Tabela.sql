de1fb93a1eeff447c30b4f505e91f9f6aee1b26c
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-21 10:33:32 -0300

    Ajustando configs do banco - Adicionando mais instruções SQL

--- SiteFechado_Agente/Banco_Tabela.sql
+++ SiteFechado_Agente/Banco_Tabela.sql
@@ -1,9 +1,19 @@
-CREATE DATABASE reporte;
+CREATE DATABASE focoNaDengue;
 
-USE reporte;
+USE focoNaDengue;
 
-CREATE TABLE dados_endereco(
-	Id INT PRIMARY KEY AUTO_INCREMENT,
-    Endereco VARCHAR(200),
-    Bairro VARCHAR(200)
+CREATE TABLE dados_reporte (
+	Id                  INT             PRIMARY KEY AUTO_INCREMENT,
+    Bairro              VARCHAR(200)    NOT NULL,
+    Endereco            VARCHAR(200)    NOT NULL
 );
+
+CREATE TABLE usuarios (
+    Id                  INT             PRIMARY KEY AUTO_INCREMENT,
+    CPF                 VARCHAR(11)     NOT NULL UNIQUE,
+    Data_aniversario    DATE            NOT NULL,
+    Senha               VARCHAR(200)    NOT NULL
+);
+
+INSERT INTO usuarios (CPF, Data_aniversario, Senha) 
+VALUES ("12345678901", '2004-04-09', "123456789")
